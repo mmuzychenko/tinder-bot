6aba42c8e00388938fcf88bfc9b30fb750548801
Maksym Muzychenko <obonemax@gmail.com>
2024-11-25 00:16:16 +0200

    project template and dependencies

---
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -0,0 +1,33 @@
+package com.javarush.telegram;
+
+import com.javarush.telegram.ChatGPTService;
+import com.javarush.telegram.DialogMode;
+import com.javarush.telegram.MultiSessionTelegramBot;
+import com.javarush.telegram.UserInfo;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+
+public class TinderBoltApp extends MultiSessionTelegramBot {
+    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: додай ім'я бота в лапках
+    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: додай токен бота в лапках
+    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: додай токен ChatGPT у лапках
+
+    public TinderBoltApp() {
+        super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        //TODO: основний функціонал бота будемо писати тут
+
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TinderBoltApp());
+    }
+}
